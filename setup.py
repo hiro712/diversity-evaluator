03f349426ba9c6b7e3784d01b77cfaff30cebb3f
hiro <abe15263076@gmail.com>
2025-06-02 15:56:20 +0900

    Solves the problem of not being able to import

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
 setuptools.setup(
     name="diversity-evaluator",
-    version="0.1.1",
+    version="0.1.2",
     author="Hiroto ABE",
     author_email="abe.hiroto.t7@dc.tohoku.ac.jp",
     description="A library to compute diversity of QUBO solutions using GED-based scoring.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/hiro712/diversity-evaluator",  # リポジトリ URL に置き換えてください
+    url="https://github.com/hiro712/diversity-evaluator",
     packages=setuptools.find_packages(),
-    python_requires=">=3.7",
-    install_requires=[
-        "networkx",
-        "numpy",
-    ],
+    install_requires=_requires_from_file('requirements.txt'),
+    python_requires=">=3.12",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
